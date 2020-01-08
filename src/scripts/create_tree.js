4d5a3c2da53c46567847be98be242ebc17438fd9
Ed Herman <edward.herman.87@gmail.com>
2020-01-08 18:53:32 -0500

    Achieve basic collapse functionality

--- src/scripts/create_tree.js
+++ src/scripts/create_tree.js
@@ -1,14 +1,16 @@
 import convertFetchedData from "./convert_fetched_data";
 
-const branchLvl = (d) => {
+const klass = (d) => {
   if (d.depth === 4) {
-    return "link upperBranches"
+    return "upper branches"
   } else if (d.depth === 3) {
-    return "link middleBranches"
+    return "middle branches"
   } else if (d.depth === 2) {
-    return "link lowerBranches"
+    return "lower branches"
+  } else if (d.depth === 4) {
+    return "leaves"
   } else {
-    return "link trunk"
+    return "trunk"
   }
 }
 
@@ -29,16 +31,14 @@ export default () => {
     }
   };
 
-  const svg = d3
+  // .data(d3.entries(orientations))
+  let svg = d3
     .select("body")
-    .selectAll("svg")
-    .data(d3.entries(orientations))
-    .enter()
     .append("svg")
-    .attr("width", width + margin.left + margin.right)
-    .attr("height", height + margin.top + margin.right)
+      .attr("width", width + margin.left + margin.right)
+      .attr("height", height + margin.top + margin.right)
     .append("g")
-    .attr("transform", "translate(" + margin.left + "," + margin.top + ")");
+      .attr("transform", "translate(" + margin.left + "," + margin.top + ")");
 
   // Load and convert csv data => each row becomes an object with columns as keys
   d3.csv("src/data/bbg_data191204.csv").then(function(data) {
@@ -46,12 +46,12 @@ export default () => {
     // Convert data to hierarchical structure
     let bbg_data = convertFetchedData(data);
 
-    svg.each(function(orientation) {
-      const svg = d3.select(this),
-        o = orientation.value;
+    // svg.each(() => {
+    //   const svg = d3.select(this)
+      
 
       // Create tree and assign size from orientations
-      let treemap = d3.tree().size(o.size);
+      let treemap = d3.tree().size([height, width]);
 
       // Assign root node
       let root = d3.hierarchy(bbg_data, d => { return d.children });
@@ -79,88 +79,75 @@ export default () => {
         const duration = 750;
 
         // Normalize depth
-        nodes.descendants().forEach(d => {d.y = d.height * 90});
-
+        nodes.descendants().forEach(d => {d.y = d.depth * 180});
         ///////// Nodes /////////
         // Update the nodes
-        // const node = svg.selectAll(".node")
-        //   .data(nodes, d => { return d.id || (d.id = ++i); })
+        let node = svg
+          .selectAll(".node")
+          .data(nodes.descendants(), d => { return d.id || (d.id = ++i); })
 
-        // Group branch and leaf nodes
-        let branchNodes = [];
-        let leafNodes = [];
-
-        nodes.descendants().forEach(node => {
-          if (node.depth === 4){
-            leafNodes.push(node);
-          } else {
-            branchNodes.push(node);
-          }
-        })
-
-        // Create branchNode circles
-        let branchNode = svg
-          .selectAll(".branchNode")
-          .data(branchNodes, d => { return d.id || (d.id = ++i); })
-
-        let branchNodeEnter = branchNode
+        // Create node circles
+        let nodeEnter = node
           .enter()
           .append("g")
-          .attr("class", "branchNode")
-          .on('click', (d) => click(d))
-          .attr("transform", d => { return `translate(${d.y}, ${d.x})`; });
+          .attr("class", d => { 
+            return `node ${klass(d)}`; })
+          .attr("transform", d => { return `translate(${source.y0}, ${source.x0})`; })
+          .on('click', (d) => click(d));
           
-        // Add Circle to branchNodes
-        branchNodeEnter
+        // Add Circle to nodes
+        nodeEnter
           .append("circle")
-          .attr("r", 4.5)
-          .attr("cx", o.x)
-          .attr("cy", o.y)
+          .attr("r", 7)
           .style("fill", d => {
-            return d._children ? "#654321" : "#fff";
+            return d.children ? "#654321" : "#fff";
           });
 
-        // Update LeafNodes
-        let leafNode = svg
-          .selectAll(".leafNode")
-          .data(leafNodes, d => { return d.id || (d.id = ++i); })
-          
-        let leafNodeEnter = leafNode  
-          .enter()
-          .append("g")
-          .attr("class", ".leafNode")
-          .on('click', d => click(d))
-          .attr("transform", d => { return `translate(${o.y}, ${o.x})`; });
-
-        // Add Circle to leafNodes
-        leafNodeEnter
-          .append("circle")
-          .attr("r", 4.5)
-          .attr("cx", o.x)
-          .attr("cy", o.y)
-          .style("fill", d => {
-            return d._children ? "forestgreen" : "#fff";
-          })
-
-        branchNode
+        // Node labels
+        nodeEnter
           .append("text")
-          .text(function(d) {
-            return d.data.name;
+          .text(d => { return d.data.name; })
+          .attr("x", d => { return d.children || d._children ? -13 : 13; })
+          .attr("dy", ".35em")
+          .attr("text-anchor", d => { return d.children || d._children ? "end" : "start"; })
+
+        // Execute updating nodes
+        const nodeUpdate = nodeEnter.merge(node);
+
+        // Transition to proper node position
+        nodeUpdate.transition()
+          .duration(duration)
+          .attr("transform", d => { return `translate(${d.y}, ${d.x})`; })
+        
+        nodeUpdate.select('.node')
+          .attr('r', 7)
+          .style("fill", d => { 
+            debugger
+            return d.children ? "forestgreen" : "#fff"; 
           })
-          .attr("x", o.x)
-          .attr("dx", 5)
-          .attr("y", o.y);
+          .attr('cursor', 'pointer');
 
+        // Remove any exiting nodes
+        let nodeExit = node.exit().transition()
+          .duration(duration)
+          .attr("transform", d => { return `translate(${source.y}, ${source.x})`; })
+          .remove();
+
+        // Reduce exiting circles size to 0
+        nodeExit.select('.node')
+          .attr('r', 1e-6);
+
+        // Reduce label opacity
+        nodeExit.select('text')
+          .style('fill-opacity', 1e-6);
+      
         ///////// Links /////////
-
-
         // Create path between parent and child
-        const diagonal = (d) => {
-          return `M ${d.x} ${o.y(d)} 
-              C ${d.x},
-              ${(o.y(d) + o.y(d.parent)) / 2} ${d.parent.x},
-              ${(o.y(d) + o.y(d.parent)) / 2} ${d.parent.x},
-              ${o.y(d.parent)}`
+        const diagonal = (s, d) => {
+          return `M ${s.y} ${s.x} 
+              C ${(s.y + d.y) / 2} ${s.x},
+              ${(s.y + d.y) / 2} ${d.x},
+              ${d.y} ${d.x}`;
         }
 
         // Update links
@@ -169,9 +156,12 @@ export default () => {
 
         // Update 'revealed' links
         let linkEnter = link.enter()
-          .append("path")
-          .attr("class", d => { return branchLvl(d); })
-          .attr("d", d => { return diagonal(d) });
+          .insert("path", "g")
+          .attr("class", d => { return `link ${klass(d)}`; })
+          .attr("d", d => { 
+            const o = {x: source.x0, y: source.y0}
+            return diagonal(o, o) 
+          });
 
         // Update
         const linkUpdate = linkEnter.merge(link);
@@ -179,12 +169,15 @@ export default () => {
         // Add transition to parent element
         linkUpdate.transition()
           .duration(duration)
-          .attr('d', d => { return diagonal(d)})
+          .attr('d', d => { return diagonal(d, d.parent); })
 
         // Remove any exiting links
         const linkExit = link.exit().transition()
           .duration(duration)
-          .attr('d', d => { return diagonal(d) })
+          .attr('d', d => { 
+            const o = {x: source.x, y: source.y}
+            return diagonal(o, o) 
+          })
           .remove();
 
         // Store old positions for transition
@@ -193,23 +186,30 @@ export default () => {
           d.y0 = d.y;
         });
 
-        // Handle click
+        // Handle click - set visibility property
         const click = d => {
           if (d.children) {
+            debugger
             d._children = d.children;
             d.children = null;
           } else {
+            debugger
             d.children = d._children;
             d._children = null;
           }
+          debugger
           update(d);
         }
       }
 
-      debugger
       root.children[0].children.forEach(collapse);
+      // root.children[0].children.forEach((child => { 
+      //   // child.children = child._children;
+      //   child._children = null;
+      // }));
+      
       update(root);
-    })
+    // })
     
   });
 }
